d2a4a67145c8dc673811dadf68d8abfa94ae8c93
Andrew <amzhou@buffalo.edu>
2019-02-15 18:10:41 -0500

    I added an xD

--- src/testing/OperationValkyria.scala
+++ src/testing/OperationValkyria.scala
@@ -2,6 +2,7 @@ package testing
 
 object OperationValkyria {
   def testCase(KappaPride : String): String = {
+    val xD: String = "ecks4"
     "I just really want to have more commits than Ben so it looks like I did more work."
   }
 }
