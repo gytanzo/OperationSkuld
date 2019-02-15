bf4fded9238d1aca3c0525e8463649758d7d7d27
Bobby <zzhao23@buffalo.edu>
2019-02-15 18:12:12 -0500

    hi

--- src/testing/OperationValkyria.scala
+++ src/testing/OperationValkyria.scala
@@ -5,5 +5,6 @@ object OperationValkyria {
     val xD: String = "ecks4"
     "I just really want to have more commits than Ben so it looks like I did more work."
     "Emilia is the best girl"
+    Ni Howdy
   }
 }
