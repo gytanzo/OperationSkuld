769e12d2088ca1cdad29d53c98b13be2d00bcbc8
Bobby <zzhao23@buffalo.edu>
2019-02-15 18:10:10 -0500

    Emilia

--- src/testing/OperationValkyria.scala
+++ src/testing/OperationValkyria.scala
@@ -3,5 +3,6 @@ package testing
 object OperationValkyria {
   def testCase(KappaPride : String): String = {
     "I just really want to have more commits than Ben so it looks like I did more work."
+    "Emilia is the best girl"
   }
 }
