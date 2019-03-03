b39692c1981186fd09f8742add5a99a49e991e5f
Bobby <zzhao23@buffalo.edu>
2019-03-03 17:11:40 -0500

    testing

--- src/ServerNetworking/Server.py
+++ src/ServerNetworking/Server.py
@@ -1,16 +1,19 @@
 from bottle import get, post, request, route, static_file, run, error
 
-@error(404)
-def error404(error):
-    return 'Bobby is good at Riven'
 
 @route('/')
 def server():
-    return static_file("page.html", root = "")
+    return static_file("page.html", root = "/FrontEnd")
 
 #@post('/join')
 #def users():
 #body for usernames
+#ex:def do_login():
+    #username = request.forms.get('username')
+    #password = request.forms.get('password')
+    #if check_login(username, password):
+     #else:
+        #return "<p>Login failed.</p>"
 #   return user #and add it the game
 
 #@post('/leave')
@@ -19,5 +22,10 @@ def server():
 #   return removed
 
 #get
+
+@error(404)
+def error404(error):
+    return "I like women with BIG personalities."
+
 
 run(host = '0.0.0.0', port = 8080, debug = True)
