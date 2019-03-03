2a14632bc20a7ff8f987b100aa758635aa581218
Andrew <amzhou@buffalo.edu>
2019-03-03 13:17:21 -0500

    Small changes before beginning coding

--- src/FrontEnd/OperationBanana.scala
+++ src/FrontEnd/OperationBanana.scala
@@ -1,5 +1,13 @@
 package FrontEnd
 
-object OperationBanana {
+import scalafx.animation.AnimationTimer
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.TextField
+import scalafx.scene.layout.GridPane
+
+object OperationBanana extends JFXApp {
+
 
 }
