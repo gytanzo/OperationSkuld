c65bef6b1a7b2829b16a354504f042242d282604
Andrew <amzhou@buffalo.edu>
2019-03-03 15:00:13 -0500

    Scrapped Scala files, made html file

---
+++ src/FrontEnd/Scala/Buttons.scala
@@ -0,0 +1,19 @@
+package FrontEnd.Scala
+
+import scalafx.Includes._
+import scalafx.event.ActionEvent
+import scalafx.scene.control.Button
+
+class ButtonGod(gameinfo: Placeholder, xScale: Double, yScale: Double) extends Button {
+  minWidth = 100 * xScale
+  minHeight = 100 * yScale
+  style = "-fx-font: 12 ariel;"
+
+}
+
+class ActionButton(val actionKey: String, game: Placeholder, xScale: Double = 1.0, yScale: Double = 1.0) extends ButtonGod(game, xScale, yScale) {
+  onAction = (event: ActionEvent) => game.useAction(actionKey)
+  text = actionKey
+  style = "-fx-font: 16 ariel"
+}
+
--- src/FrontEnd/Scala/Placeholder.scala
+++ src/FrontEnd/Scala/Placeholder.scala
@@ -1,6 +1,9 @@
-package FrontEnd
+package FrontEnd.Scala
 
 class Placeholder {
 // This is an empty class created to be put into the place of the game file in OperationBanana.scala and Buttons.scala
+// Nothing here matters. Nothing here will ever matter.
+  def useAction(actionKey: String): Unit ={
 
+  }
 }
