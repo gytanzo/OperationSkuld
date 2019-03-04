3e1db78a8bc45210e42303a5f506a28f88a2f273
Bobby <zzhao23@buffalo.edu>
2019-03-04 14:40:14 -0500

    Trying to set up xhttp

--- src/Website/Server.py
+++ src/Website/Server.py
@@ -5,7 +5,7 @@ from bottle import get, post, request, route, static_file, run, error
 def server():
     return static_file("page.html", root = "")
 
-#@post('/Game/join')
+#@post('/join')
 #def addUsers():
 #    new_user = {"Username" : request.json.get('Username'), 'Status' : request.json.get('Status')}
 #    User.append(new_user)
@@ -15,13 +15,14 @@ def server():
 #Username : namess and Status  for json string ingame, quit
 
 
-#@get('/Game/<all>')
+#@get('/all')
 #def allUsers():
-#    return {}
+#    all_user =
+ #   return {"Username" : Username, "Stats", Status}
 
-#@error(404)
-#def error404(error):
-#    return "You should watch A Place Further Than The Universe."
+@error(404)
+def error404(error):
+    return "You should watch A Place Further Than The Universe."
 
 
 run(host = '0.0.0.0', port = 8080, debug = True)
